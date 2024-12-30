d653def38868e7e1ed6ce1056fa8132f59c75dc5
Akhilesh Yadav <ydvakhilesh16@gmail.com>
2024-12-30 14:22:27 +0530

    options added

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ app.use(cors({
     credentials: true,
 }));
 
+// Enable preflight requests for all routes
+app.options('*', cors());
+
+// Debugging middleware to log requests
+app.use((req, res, next) => {
+    console.log('Request received: ', req.method, req.url);
+    next();
+});
 //import routes for Todo API
 const todoRoutes = require('./routes/todos');
 
